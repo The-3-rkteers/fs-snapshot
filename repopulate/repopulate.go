f8f275b5e0c09e71093869f122ad0cbcccadf304
grantseltzer <grantseltzer@gmail.com>
2016-11-13 03:23:15 -0500

    Repopulate
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- repopulate/repopulate.go
+++ repopulate/repopulate.go
@@ -1,1 +1,21 @@
 package repopulate
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/The-3-rkteers/fs-snapshot/dirtree"
+)
+
+// LoadSnapshot loads the snapshot json file into memory
+func LoadSnapshot(fileName string) (interface{}, error) {
+	jsonString, err := ioutil.ReadFile(fileName)
+	jsonBytes := []byte(jsonString)
+	var root dirtree.Directory
+	err = json.Unmarshal(jsonBytes, root)
+	return jsonBytes, err
+}
+
+func CreateDirectory() {
+
+}
